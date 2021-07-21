2c0743bd4b4096754498a87c4066129ac32b8caf
“nitrolo” <nitrolo225@gmail.com>
2021-07-22 03:32:33 +0530

    added projects section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import AboutSection from '../components/AboutSection';
 import HeroSection from '../components/HeroSection';
+import ProjectsSection from '../components/ProjectsSection';
 
 function Home() {
   return (
     <div>
       <HeroSection />
       <AboutSection />
+      <ProjectsSection />
     </div>
   );
 }
